aeb335acfcbb9df32eb88d610f8ec6d8332ebabc
Gurpreetwork9582 <gurpreet.singh@bmmca.internal>
2026-05-08 15:26:19 -0300

    Added schema validator in util folder, changed functions

--- tests/api/test_posts.py
+++ tests/api/test_posts.py
@@ -1,10 +1,57 @@
 import requests
+from utils.schema_validator import Validate_schema
+
 
 
 
 Baseurl = "https://jsonplaceholder.typicode.com/"
+Schema_path_posts="https://jsonplaceholder.typicode.com/posts"
+
+
 
 
 class Test_jsonplaceholder():
-    def test_posts():
+        
+            
+    def test_get_website(self):
+        self.response = requests.get(Baseurl)
+        assert self.response.status_code == 200
+        
+    def test_get_posts_1(self):
+        self.posts = "posts"
+        self.posts_id=1
+        self.response=requests.get(f"{Baseurl}/{self.posts}/{self.posts_id}")
+        print(self.response.json())
+        assert Validate_schema(self.response,"https://jsonplaceholder.typicode.com/posts/1")
+        
+    def test_post_posts_new(self):
+        self.posts = "posts"
+        self.data = {"userId":10,"title":"Testing the post requests"}
+        self.response=requests.post(f"{Baseurl}/posts", json=self.data)
+        assert self.response.status_code == 201
+        print(self.response.json())
+        assert self.response.json()['userId'] == 10
+
+    def test_post_Update(self):
+        self.data = {"title":"New line"}
+        self.reponse = requests.patch(f"{Baseurl}/posts/101",data=self.data)
+        print(self.response.json())
+        self.reponse_json=self.response.json()
+        assert self.reponse_json["title"] == self.data["title"]
+
+    def test_delete_post(self):
+        self.response=requests.delete(f"{Baseurl}/posts/1")
+        assert self.response.status_code == 200
+        assert self.response.json()== {}
+        assert self.reponse.elapsed.total_seconds() < 2
+               
+        
+        
+scanner = Test_jsonplaceholder()
+#scanner.test_get_website()
+scanner.test_get_posts_1()
+#scanner.test_post_posts_new()
+#scanner.test_post_Update()
+#scanner.test_delete_post()
+
         
---
+++ utils/schema_validator.py
@@ -0,0 +1,10 @@
+import json
+from jsonschema  import  validate
+
+
+def Validate_schema(reponse_json,schema_path): # creating a validator for Schema
+    with open(schema_path) as file:             # Opening Schema file
+            schema = json.load(file)            # loading it as json(now we have the json file formate how out reponse should look like)
+
+
+    validate(instance=reponse_json, schema=schema)  # validating the reponse we get and how it should look like with schema above from schema
